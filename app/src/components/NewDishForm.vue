346d747647e556c31761362cf6c04135addc9a2b
Yevhen <shershen.yevhen@gmai>
2023-05-11 20:44:57 +0200

    Autofocus Practice in component NewDishForm

--- app/src/components/NewDishForm.vue
+++ app/src/components/NewDishForm.vue
@@ -1,17 +1,27 @@
-<script>
-import { defineComponent, ref } from 'vue'
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
-
-export default defineComponent({
-  emits: ['add-new-dish', 'cancel-new-dish'],
-  data: () => ({
-    newDish: {
-      id: uuidv4(),
-      name: '',
-      status: 'Want to Try',
-      diet: '',
-    },
-  }),
+import type { Dish } from '@/types'
+const emit = defineEmits<{
+  (e: 'add-new-dish', dish: Dish): void
+  (e: 'cancel-new-dish'): void
+}>()
+// emits: ['add-new-dish', 'cancel-new-dish'],
+const newDish = ref<Dish>({
+  id: uuidv4(),
+  name: '',
+  status: 'Want to Try',
+  diet: '',
+})
+const elNameInput = ref<HTMLInputElement | null>(null)
+const addNewDish = () => {
+  emit('add-new-dish', newDish.value)
+}
+const cancelNewDish = () => {
+  emit('cancel-new-dish')
+}
+onMounted(() => {
+  elNameInput.value?.focus()
 })
 </script>
 
@@ -33,8 +43,8 @@ export default defineComponent({
       </div>
       <div class="field">
         <div class="buttons">
-          <button @click="$emit('add-new-dish', newDish)" class="button is-success">Create</button>
-          <button @click="$emit('cancel-new-dish')" class="button is-light">Cancel</button>
+          <button @click="addNewDish" class="button is-success">Create</button>
+          <button @click="cancelNewDish" class="button is-light">Cancel</button>
         </div>
       </div>
     </div>
