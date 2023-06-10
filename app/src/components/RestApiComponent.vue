30901db8b891eb0e270dec08ade86f54459c17c3
Yevhen <shershen.yevhen@gmai>
2023-06-10 11:11:18 +0200

    transfer data by the props from parent component to child

--- app/src/components/RestApiComponent.vue
+++ app/src/components/RestApiComponent.vue
@@ -1,23 +1,34 @@
 <!-- how hooks work https://www.youtube.com/watch?v=hk6OktYj8Fs&list=PLC3y8-rFHvwgeQIfSDtEGVvvSEPDkL_1f&index=48 -->
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import LoadPosts from './rest-api/LoadPosts.vue'
-import PostPosts from './rest-api/PostPsots.vue'
-import DeletePosts from './rest-api/DeletePosts.vue'
-import LoadUsers from './rest-api/LoadUsers.vue'
+import PostPosts from './rest-api/PostPosts.vue'
+// import DeletePosts from './rest-api/DeletePosts.vue'
+// import LoadUsers from './rest-api/LoadUsers.vue'
+import { loadPosts } from '@/services/posts.services'
+const posts = ref([])
+const getPosts = async () => {
+  const data = await loadPosts()
+  if (!!data) posts.value = data
+}
+onMounted(() => {
+  getPosts()
+})
 </script>
 
 <template>
   <div>
     <h1>REST API</h1>
     <div class="select">
-      <LoadPosts />
+      <LoadPosts :posts="posts" />
       <hr />
       <PostPosts />
+      <!--
       <hr />
       <DeletePosts />
       <hr />
-      <LoadUsers />
+      <LoadUsers /> -->
     </div>
   </div>
 </template>
--- app/src/components/rest-api/LoadPosts.vue
+++ app/src/components/rest-api/LoadPosts.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { loadPosts } from '@/services/posts.services'
-const posts = ref('')
+const props = defineProps<{
+  posts?: any
+}>()
+const posts = ref([])
 const errorMessage = ref('')
-const showPostList = ref(false)
-const getPosts = async () => {
-  const data = await loadPosts()
-  if (!!data) {
-    posts.value = data
-    showPostList.value = !showPostList.value
+const postList = ref(false)
+const showPostList = () => {
+  posts.value = props.posts
+  if (!!posts) {
+    postList.value = !postList.value
     errorMessage.value = ''
   } else errorMessage.value = 'Your request to server is wrong!'
 }
@@ -16,10 +17,10 @@ const getPosts = async () => {
 <template>
   <div>
     <h1>GET DATA</h1>
-    <button @click="getPosts()">Load Posts</button>
+    <button @click="showPostList()">Load Posts</button>
     <h2 v-if="errorMessage">{{ errorMessage }}</h2>
-    <h2 v-if="showPostList">POST LIST</h2>
-    <ul v-if="showPostList">
+    <h2 v-if="postList">POST LIST</h2>
+    <ul v-if="postList">
       <li v-for="post in posts" :key="post">{{ post.id }}: {{ post.title }}</li>
     </ul>
   </div>
