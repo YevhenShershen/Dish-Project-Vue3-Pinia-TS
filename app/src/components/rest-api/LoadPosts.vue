05466a7fa70695173437997fe46588607ae514fb
Yevhen <shershen.yevhen@gmai>
2023-06-10 10:32:38 +0200

    add variable errorMessage and change constant name in component LoadPosts.vue

--- app/src/components/rest-api/LoadPosts.vue
+++ app/src/components/rest-api/LoadPosts.vue
@@ -3,13 +3,14 @@ import { ref } from 'vue'
 import { loadPosts } from '@/services/posts.services'
 const posts = ref('')
 const errorMessage = ref('')
-const showData = ref(false)
+const showPostList = ref(false)
 const getPosts = async () => {
   const data = await loadPosts()
   if (!!data) {
     posts.value = data
-    showData.value = !showData.value
-  }
+    showPostList.value = !showPostList.value
+    errorMessage.value = ''
+  } else errorMessage.value = 'Your request to server is wrong!'
 }
 </script>
 <template>
@@ -17,8 +18,8 @@ const getPosts = async () => {
     <h1>GET DATA</h1>
     <button @click="getPosts()">Load Posts</button>
     <h2 v-if="errorMessage">{{ errorMessage }}</h2>
-    <h2 v-if="posts">POST LIST</h2>
-    <ul v-if="showData">
+    <h2 v-if="showPostList">POST LIST</h2>
+    <ul v-if="showPostList">
       <li v-for="post in posts" :key="post">{{ post.id }}: {{ post.title }}</li>
     </ul>
   </div>
